4086f57150d994ec534e4a64072a559ac4d73367
eyan422 <eyan422eyan422@gmail.com>
2014-05-15 16:11:42 +0800

    hashMap

---
+++ Data Structure With JAVA/Collections/TestDictionary/src/testDictionary/TestDictionary.java
@@ -0,0 +1,26 @@
+package testDictionary;
+
+import java.util.*;
+
+public class TestDictionary {
+	public static void main(String[] args) {
+		
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("Tag","day");
+		map.put("Hut","hat");
+		map.put("Uhr","clock");
+		map.put("Rad","wheel");
+		map.put("Ohr","ear");
+		map.put("Tor","gate");
+		
+		System.out.println("map=" + map);
+		System.out.println("map.size()=" + map.size());
+		System.out.println("map.keySet()=" + map.keySet());
+		System.out.println("map.values()=" + map.values());
+		System.out.println("map.get(\"Uhr\")=" + map.get("Uhr"));
+		System.out.println("map.remove(\"Rad\")=" + map.remove("Rad"));
+		System.out.println("map.get(\"Rad\")=" + map.get("Rad"));
+		System.out.println("map=" + map);
+		System.out.println("map.size()=" + map.size());
+	}
+}
