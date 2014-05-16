5ed94bdd25e0458c6e9b95be485c53634f2e3f90
eyan422 <eyan422eyan422@gmail.com>
2014-05-16 11:10:29 +0800

    Concordance test file location change

--- Data Structure With JAVA/Collections/Concordance/src/concordance/TestConcordance.java
+++ Data Structure With JAVA/Collections/Concordance/src/concordance/TestConcordance.java
@@ -1,7 +1,8 @@
 package concordance;
 
 public class TestConcordance {
-	public static final String PATH = "C:\\Users\\UFIS-Frank\\Desktop\\";
+	//public static final String PATH = "C:\\Users\\UFIS-Frank\\Desktop\\";
+	public static final String PATH = "D:\\Git\\PersonalProject\\Data Structure With JAVA\\Collections\\Concordance\\src\\concordance\\";
 	public static final String IN_FILE = "cases.txt";
 	public static final String OUT_FILE = "cases.out";
 	
