5c409c1cc808b7f0ce403f4b14c775bec176e959
eyan422 <eyan422eyan422@gmail.com>
2014-05-16 11:05:25 +0800

    Concordance

---
+++ Data Structure With JAVA/Collections/Concordance/src/concordance/TestConcordance.java
@@ -0,0 +1,12 @@
+package concordance;
+
+public class TestConcordance {
+	public static final String PATH = "C:\\Users\\UFIS-Frank\\Desktop\\";
+	public static final String IN_FILE = "cases.txt";
+	public static final String OUT_FILE = "cases.out";
+	
+	public static void main(String[] args) {
+		Concordance c = new Concordance(PATH+IN_FILE);
+		c.write(PATH+OUT_FILE);
+	}
+}
