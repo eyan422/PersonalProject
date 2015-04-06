032ce034d789aaf4a2404c840297b116b2494ca1
eyan422 <eyan422eyan422@gmail.com>
2015-04-06 12:16:59 +0800

    Chapter 4-2

--- OCA Java SE 7 Programmer I Certification Guide/Equility/src/equility/Equility.java
+++ OCA Java SE 7 Programmer I Certification Guide/Equility/src/equility/Equility.java
@@ -7,10 +7,12 @@ public class Equility {
 		String var2 = new String("Java");
 		System.out.println(var1.equals(var2));
 		System.out.println(var1 == var2);
+		System.out.println(var1 != var2);
 		
 		String var3 = "code";
 		String var4 = "code";
 		System.out.println(var3.equals(var4));
 		System.out.println(var3 == var4);
+		System.out.println(var3 != var4);
 	}
 }
