abea13513072d7d2c6cdb4a2f6fc8133e0ecad99
eyan422 <eyan422eyan422@gmail.com>
2015-04-09 18:13:04 +0800

    ArrayList-1

---
+++ OCA Java SE 7 Programmer I Certification Guide/MiscMethodsArrayList3/src/miscmethodsarraylist3/MiscMethodsArrayList3.java
@@ -0,0 +1,23 @@
+package miscmethodsarraylist3;
+import java.util.ArrayList;
+
+public class MiscMethodsArrayList3 {
+	public static void main(String args[]) {
+		ArrayList<StringBuilder> myArrList =new ArrayList<StringBuilder>();
+		
+		StringBuilder sb1 = new StringBuilder("Jan");
+		StringBuilder sb2 = new StringBuilder("Feb");
+		
+		myArrList.add(sb1);
+		myArrList.add(sb2);
+		myArrList.add(sb2);
+		
+		System.out.println(myArrList.contains(new StringBuilder("Jan")));
+		System.out.println(myArrList.contains(sb1));
+		
+		System.out.println(myArrList.indexOf(new StringBuilder("Feb")));
+		System.out.println(myArrList.indexOf(sb2));
+		System.out.println(myArrList.lastIndexOf(new StringBuilder("Feb")));
+		System.out.println(myArrList.lastIndexOf(sb2));
+	}
+}
