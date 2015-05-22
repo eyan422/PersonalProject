83658ce74d95049af3b7a653e725e9feddc984b9
eyan422 <eyan422eyan422@gmail.com>
2015-05-22 10:30:55 +0800

    Array Init

---
+++ OCA Java SE 7 Programmer I Certification Guide/ArrayInit/src/arrayinit/ArrayInit.java
@@ -0,0 +1,15 @@
+package arrayinit;
+
+public class ArrayInit {
+	public static void main(String args[])
+	{
+		int eArr1[] = {10, 23, 10, 2};
+		int[] eArr2 = new int[10];
+		int[] eArr3 = new int[] {};
+		
+		/* Error Initialization
+		int[] eArr4 = new int[10] {};
+		int eArr5[] = new int[2] {10, 20};
+		*/
+	}
+}
