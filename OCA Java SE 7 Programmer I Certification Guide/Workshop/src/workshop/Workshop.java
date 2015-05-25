a50ee4e4e273a56041ca20c52ec071a3dc735dcf
eyan422 <eyan422eyan422@gmail.com>
2015-05-25 13:43:20 +0800

    worksjop

--- OCA Java SE 7 Programmer I Certification Guide/Workshop/src/workshop/Workshop.java
+++ OCA Java SE 7 Programmer I Certification Guide/Workshop/src/workshop/Workshop.java
@@ -6,12 +6,16 @@ class Laptop {
 
 class Workshop {
 	public static void main(String args[]) {
+		
 		Laptop life = new Laptop();
+		
 		repair(life);
+		
 		System.out.println(life.memory);
 	}
 	
 	public static void repair(Laptop laptop) {
+		laptop = new Laptop();
 		laptop.memory = "2GB";
 	}
 }
