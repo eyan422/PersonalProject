05089a410971f6b3cb7f1530ded36daf7e7ad104
eyan422 <eyan422eyan422@gmail.com>
2015-05-27 01:10:26 +0800

    double int

---
+++ OCA Java SE 7 Programmer I Certification Guide/double_int/src/double_int/Double_int.java
@@ -0,0 +1,12 @@
+package double_int;
+
+public class Double_int {
+	public static void main(String... args)
+	{
+		double d = 10.0;
+		System.out.println(d);
+		d = 9;
+		
+		System.out.println(d);
+	}
+}
