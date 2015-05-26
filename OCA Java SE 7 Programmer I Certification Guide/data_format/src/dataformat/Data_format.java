83bc6dd33930e61fd94e9790f1d1222eae003205
eyan422 <eyan422eyan422@gmail.com>
2015-05-26 18:42:18 +0800

    data format

---
+++ OCA Java SE 7 Programmer I Certification Guide/data_format/src/dataformat/Data_format.java
@@ -0,0 +1,18 @@
+package dataformat;
+
+public class Data_format {
+	public static void main(String[] args)
+	{
+		long var1 = 0_100_267_760;
+		//long var2 = 0_x_4_13;
+		//long var3 = 0b_x10_BA_75;
+		//long var4 = 0b_10000_10_11;
+		//long var5 = 0xa10_AG_75;
+		long var6 = 0x1_0000_10;
+		long var7 = 100__12_12;
+		
+		System.out.println(var1);
+		System.out.println(var6);
+		System.out.println(var7);
+	}
+}
