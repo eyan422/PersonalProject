26ace0215db3c085e222366096e5d93a587e7203
eyan422 <eyan422eyan422@gmail.com>
2015-05-27 18:27:22 +0800

    startof

--- OCA Java SE 7 Programmer I Certification Guide/ConstructerIni/src/constructerini/ConstructerIni.java
+++ OCA Java SE 7 Programmer I Certification Guide/ConstructerIni/src/constructerini/ConstructerIni.java
@@ -20,6 +20,47 @@ class Employee {
 	{
 		System.out.println("ini block");
 	}
+	
+	void test(int i)
+	{
+		System.out.println("i");
+	}
+	
+	void test(float l)
+	{
+		System.out.println("f");
+	}
+	
+	void test(double l)
+	{
+		System.out.println("d");
+	}
+	
+	void test(byte l)
+	{
+		System.out.println("b");
+	}
+	
+	void test(long l)
+	{
+		System.out.println("l");
+	}
+	
+	double calcAverage(double marks1, int marks2) {
+		return (marks1 + marks2)/2.0;
+	}
+	
+	double calcAverage(int marks1, double marks2) {
+		return (marks1 + marks2)/2.0;
+	}
+	
+	/*
+	char[] test()
+	{
+		char x[] = null;
+		return x;
+	}
+	*/
 }
 
 class ConstructerIni
@@ -30,5 +71,10 @@ class ConstructerIni
 		Employee e = new Employee();
 		System.out.println(e.name);
 		System.out.println(e.age);
+		
+		e.test(10);
+		e.test(1.0f);
+		
+		//e.calcAverage(2, 3);
 	}
 }
