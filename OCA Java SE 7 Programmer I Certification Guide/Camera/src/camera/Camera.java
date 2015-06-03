c499444f57da3f1fec7a33ae77096d16c2295fe9
eyan422 <eyan422eyan422@gmail.com>
2015-06-03 15:42:06 +0800

    Camera

---
+++ OCA Java SE 7 Programmer I Certification Guide/Camera/src/camera/Camera.java
@@ -0,0 +1,23 @@
+package camera;
+
+class Camera {
+	public static void main(String args[]) {
+	String settings;
+	boolean flag = false;
+	
+	while (flag) {
+		settings = "Adjust settings manually1";
+	}
+	
+	if(flag)
+	{
+		settings = "Adjust settings manually2";
+	}
+	else
+	{
+		settings = "Adjust settings manually3";
+	}
+	
+	System.out.println("Camera:" + settings);
+	}
+}
