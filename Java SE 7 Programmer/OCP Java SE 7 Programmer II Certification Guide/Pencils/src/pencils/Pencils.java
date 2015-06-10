b16798d0943e9aa180a805ece204a7e7412f65ce
eyan422 <eyan422eyan422@gmail.com>
2015-06-10 14:27:31 +0800

    static initializer

--- Java SE 7 Programmer/OCP Java SE 7 Programmer II Certification Guide/Pencils/src/pencils/Pencils.java
+++ Java SE 7 Programmer/OCP Java SE 7 Programmer II Certification Guide/Pencils/src/pencils/Pencils.java
@@ -4,12 +4,24 @@ class Instrument {
 	Instrument() {
 		System.out.println("Instrument:constructor");
 	}
+	{
+		System.out.println("Instrument: initializer");
+	}
+	
+	static {
+		System.out.println("Instrument: init");
+	}
 }
 
 class Pencils extends Instrument {
 	public Pencils() {
 		System.out.println("Pencil:constructor");
 	}
+	
+	static {
+		System.out.println("Pencils: init");
+	}
+	
 	{
 		System.out.println("Pencil:instance initializer");
 	}
